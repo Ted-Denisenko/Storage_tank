5b21a30759f53fd9d57cc2502b31fe3ce45ea6cb
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-16 11:50:28 +0600

    Реализован ввод содержимого резервуара fix #1

--- Storage_tank/Storage_tank.cpp
+++ Storage_tank/Storage_tank.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+// Необходимо реализовать ввод через консоль уровня содержимого резервуара в мм.
+void levelInput()
+{
+	int level;
+	std::cout << "Input tank content level" << std::endl << "Level = ";
+	std::cin >> level;
+}
+
 int main()
 {
-
 }
