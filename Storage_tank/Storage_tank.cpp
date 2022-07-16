9bcdcc790c9b41f0447398daf5bc6d70a2cfa62d
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-16 12:01:08 +0600

    levelInput() теперь позволяет работать с введенным значением Ввод уровня вызывается при запуске программы автоматически fix #1

--- Storage_tank/Storage_tank.cpp
+++ Storage_tank/Storage_tank.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
 // Необходимо реализовать ввод через консоль уровня содержимого резервуара в мм.
-void levelInput()
+int levelInput()
 {
 	int level;
-	std::cout << "Input tank content level" << std::endl << "Level = ";
+	std::cout << "Input tank content level (in mm)" << std::endl << "Level = ";
 	std::cin >> level;
+	return level;
 }
 
 int main()
 {
+	levelInput();
 }
