9a4d3f1c96de35d1e98fad3b6cbf1474e1bcc5e0
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-16 14:17:37 +0600

    Реализованы тесты: Состав тестов (случай - результат) Тесты объема: - 1000 - 6,28 - 8000 - 50,24 - 1 - 0,00628000032
    Тесты массы:
    - 6,28 - 6531.20020
    - 50,42 - 52249.6
    - 0.00628000032 - 6.5312003328
    fix #4

--- Storage_tank/Storage_tank.cpp
+++ Storage_tank/Storage_tank.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 // Необходимо реализовать ввод через консоль уровня содержимого резервуара в мм.
 int LevelInput()
@@ -9,19 +10,46 @@ int LevelInput()
 	return level;
 }
 
-void MassValue(int level)
+float ContentValue(int level)
+{
+	int tankDiameter = 4; //meters
+	float contentValue = 3.14 * (tankDiameter) / 2 * (float(level) / float(1000));
+	std::cout << "\nValue of content is " << contentValue << std::endl;
+	return contentValue;
+}
+
+float ContentMass(float contentValue)
 {
 	int contentDensity = 1040; //kg/m^3
-	int tankDiameter = 4; //meters
-
-	float contentValue = 3.14 * (tankDiameter) / 2 * (level / 1000);
 	float contentMass = contentValue * contentDensity;
-
-	std::cout << "\nValue of content is " << contentValue << std::endl;
 	std::cout << "Mass of content is " << contentMass << std::endl;
+	return contentMass;
+}
+
+void Tests()
+{
+	float valueExpected{-1};
+	valueExpected = 6.28;
+	assert(ContentValue(1000) == valueExpected && "Value test failed!");
+	valueExpected = 50.24;
+	assert(ContentValue(8000) == valueExpected && "Value test failed!");
+	valueExpected = 0.00628000032;
+	assert(ContentValue(1) == valueExpected && "Value test failed!");
+	std::cout << "Value tests passed" << std::endl;
+
+	float massExpected{ -1 };
+	massExpected = 6531.20020;
+	assert(ContentMass(ContentValue(1000)) == massExpected && "Mass test failed!");
+	massExpected = 52249.6;
+	assert(ContentMass(ContentValue(8000)) == massExpected && "Mass test failed!");
+	massExpected = 6.5312003328;
+	assert(ContentMass(ContentValue(1)) == massExpected && "Mass test failed!");
+	std::cout << "Mass tests passed" << std::endl;
 }
 
 int main()
 {
-	MassValue(LevelInput());
+	Tests();
+	ContentMass(ContentValue(LevelInput()));
+	
 }
