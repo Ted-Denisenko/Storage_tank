0377ecc27db529157cd567c977326f72f718cc47
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-16 12:22:14 +0600

    Добавлены вычисления объема содержимого и его массы fix #2

--- Storage_tank/Storage_tank.cpp
+++ Storage_tank/Storage_tank.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // Необходимо реализовать ввод через консоль уровня содержимого резервуара в мм.
-int levelInput()
+int LevelInput()
 {
 	int level;
 	std::cout << "Input tank content level (in mm)" << std::endl << "Level = ";
@@ -9,7 +9,16 @@ int levelInput()
 	return level;
 }
 
+void MassValue(int level)
+{
+	int contentDensity = 1040; //kg/m^3
+	int tankDiameter = 4; //meters
+
+	float contentValue = 3.14 * (tankDiameter) / 2 * (level / 1000);
+	float contantMass = contentValue * contentDensity;
+}
+
 int main()
 {
-	levelInput();
+	MassValue(LevelInput());
 }
