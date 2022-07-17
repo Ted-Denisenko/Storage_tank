6a31fdb707f8dd14940b42a8330d3a2760714d25
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-17 16:48:23 +0600

    Убран тест с необоснованным условием (fix #8) Заменены проверки с BOOST_REQUIRE  на BOOST_CHECK_CLOSE для сравнения чисел с плавающей точкой (fix #10, fix#11)

--- Storage_tank/UnitTests.cpp
+++ Storage_tank/UnitTests.cpp
@@ -1,11 +1,13 @@
 #include "Storage_tank.h"
-#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_MODULE Storage_tank.UNIT_TESTS
 #include <boost/test/unit_test.hpp>
+namespace utf = boost::unit_test;
 
-BOOST_AUTO_TEST_CASE(my_test)
+
+BOOST_AUTO_TEST_CASE(my_test, *utf::tolerance(0.00001))
 {
-	BOOST_REQUIRE(ContentValue(1000) == 6.28000021);
-	BOOST_REQUIRE(ContentValue(1) == 0.00628000032);
-	BOOST_REQUIRE(ContentMass(ContentValue(1000)) == 6531.20020);
-	BOOST_REQUIRE(ContentMass(ContentValue(1)) == 6.53120041);
+	BOOST_CHECK_CLOSE(ContentValue(1000), 6.28000021, 0000001);
+	BOOST_CHECK_CLOSE(ContentValue(1), 0.00628000032, 0000001);
+	BOOST_CHECK_CLOSE(ContentMass(ContentValue(1000)), 6531.20020, 0000001);
+	BOOST_CHECK_CLOSE(ContentMass(ContentValue(1)), 6.53120041, 0000001);
 }
