c915d84ffa5118a944bbf838e43f266d2437d6c6
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-17 17:10:33 +0600

    У каждого теста теперь свой BOOST_AUTO_TEST_CASE со своим именем (формат: тест_функция_аргумент) fix #12

--- Storage_tank/UnitTests.cpp
+++ Storage_tank/UnitTests.cpp
@@ -4,10 +4,22 @@
 namespace utf = boost::unit_test;
 
 
-BOOST_AUTO_TEST_CASE(my_test, *utf::tolerance(0.00001))
+BOOST_AUTO_TEST_CASE(test_ContentValue_1000)
 {
 	BOOST_CHECK_CLOSE(ContentValue(1000), 6.28000021, 0000001);
+}
+
+BOOST_AUTO_TEST_CASE(test_ContentValue_1)
+{
 	BOOST_CHECK_CLOSE(ContentValue(1), 0.00628000032, 0000001);
+}
+
+BOOST_AUTO_TEST_CASE(test_ContentMass_1000)
+{
 	BOOST_CHECK_CLOSE(ContentMass(ContentValue(1000)), 6531.20020, 0000001);
+}
+
+BOOST_AUTO_TEST_CASE(test_ContentMass_1)
+{
 	BOOST_CHECK_CLOSE(ContentMass(ContentValue(1)), 6.53120041, 0000001);
 }
