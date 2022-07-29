4983ad21f9c7b3daa978b625fd6e0db675d6f7f1
Ted-Denisenko <91020107+Ted-Denisenko@users.noreply.github.com>
2022-07-29 12:18:22 +0700

    Сборка debug теперь собирается и работает нормально fix #34

--- Storage_tank/main.cpp
+++ Storage_tank/main.cpp
@@ -5,16 +5,16 @@ using namespace boost::program_options;
 
 int main(int argc, const char* argv[])
 {
+    
+    options_description desc{ "Options" };
+    desc.add_options()
+        ("help,h", "Help screen")
+        ("diameter,diam", value<int>()->required()->default_value(500), "Tank diameter")
+        ("height,hght", value<int>()->required()->default_value(1000), "Tank height")
+        ("level,lvl", value<int>()->required()->default_value(600), "Content level")
+        ("density, dens", value<double>()->required()->default_value(1040), "Content density");
     try
     {
-        options_description desc{ "Options" };
-        desc.add_options()
-            ("help,h", "Help screen")
-            ("diameter,diam", value<int>()->default_value(500), "Tank diameter")
-            ("height,hght", value<int>()->default_value(1000), "Tank height")
-            ("level,lvl", value<int>()->default_value(600), "Content level")
-            ("density, dens", value<int>()->default_value(1040), "Content density");
-
         variables_map vm; // в мапе всегда будет столько пар название-значение, сколько значений по умолчанию выставлено
         store(parse_command_line(argc, argv, desc), vm);
 
@@ -23,7 +23,7 @@ int main(int argc, const char* argv[])
         else
         {
             ContentMass(
-                        ContentValue(vm.at("level").as<int>(), vm.at("height").as<int>(), vm.at("diameter").as<int>())
+                        ContentValue(vm["level"].as<int>(), vm["height"].as<int>(), vm["diameter"].as<int>())
                         , vm.at("density").as<double>());
         }
         
